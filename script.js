91c69a5deccde616cd83d220795d3b82f369b6a2
JJT-SK <mrjackdooney@gmail.com>
2025-04-10 23:11:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add smooth scroll behavior (if needed later)
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
