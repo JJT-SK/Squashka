25e7abbfa0d3aa13cbe7335d785d4149e54bc9cd
JJT-SK <mrjackdooney@gmail.com>
2025-05-10 18:00:07 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add smooth scroll behavior (if needed later)
-document.querySelectorAll("a[href^='#']").forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
-  });
-});
